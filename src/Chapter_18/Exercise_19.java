0a9bc10982dc7d917a48de9f49ba8cca54508218
LuizGsa21 <LArantesSa@icloud.com>
2014-12-02 22:48:50 -0600

    Remove reduntant imports "javafx textfield && label"

--- src/Chapter_18/Exercise_19.java
+++ src/Chapter_18/Exercise_19.java
@@ -6,8 +6,6 @@ import javafx.geometry.Point2D;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
