676750463181be4af36b9d3b2a065da3c293ba45
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-01-30 18:57:22 -0600

    Added addAll, removeAll, and retainAll methods
    	Part of Ch24 Exercise 1

--- src/Chapter_24/MyList.java
+++ src/Chapter_24/MyList.java
@@ -43,4 +43,15 @@ public interface MyList<E> extends Iterable<E> {
 
   /** Return the number of elements in this list */
   public int size();
+
+  /** Adds the elements in otherList to this list.
+   * Returns true if this list changed as a result of the call */
+  public boolean addAll(MyList<E> otherList);
+
+  /** Removes all the elements in otherList from this list
+   * Returns true if this list changed as a result of the call */
+  public boolean removeAll(MyList<E> otherList);
+
+  /** Retains the elements in this list that are also in otherList * Returns true if this list changed as a result of the call */
+  public boolean retainAll(MyList<E> otherList);
 }
